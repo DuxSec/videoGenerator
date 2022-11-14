0950d4914fb395ba4e26a61592a34afa36034a9c
fabrree <80321003+fabrree@users.noreply.github.com>
2022-11-14 18:27:00 +0100

    cleanup code part 1

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 from tqdm.auto import tqdm
 from moviepy.editor import *
 import os
+import glob
 from dotenv import load_dotenv
 from moviepy.video.io.VideoFileClip import VideoFileClip
 from mutagen.mp3 import MP3
@@ -18,7 +19,7 @@ PEXELS_API_KEY = os.getenv('PEXELS_API_KEY') #add your api key in .ENV
 
 
 # download background video from pexels - https://www.pexels.com/api/documentation/#videos-search__parameters
-def downloadVideo(id):
+def downloadVideo(id) -> str:
     """Downloads video from Pexels with the according video ID """
     url = "https://www.pexels.com/video/" + str(id) + "/download.mp4"
     # Streaming, so we can iterate over the response.
@@ -26,7 +27,7 @@ def downloadVideo(id):
     total_size_in_bytes= int(response.headers.get('content-length', 0))
     block_size = 1024 #1 Kibibyte
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-    save_as = "vid.mp4" # the name you want to save file as
+    save_as = "tempFiles/vid.mp4" # the name you want to save file as
     with open(save_as, 'wb') as file:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
@@ -61,13 +62,21 @@ def scrapeVideos():
         exit()
     return data
 
-def deleteFirstLineInTxt():
-    """Removes the quote text we used in the video, so we don't have duplicates"""
+def usedQuoteToDifferentFile():
+    """Removes the used quote from the .txt and places the quote in usedQuotes.txt"""
+    quote = None
     with open('quotes/motivational.txt', 'r+') as file:
         lines = file.readlines()
+        quote = lines[0]
         file.seek(0)
         file.truncate()
         file.writelines(lines[1:])
+    
+    with open('quotes/usedQuotes.txt', 'a') as file:
+        file.write(quote)
+
+
+    
 
 def getQuote():
     """Get 1 quote from the text file"""
@@ -77,27 +86,21 @@ def getQuote():
         print ("Quote: ", x)
         return x
 
-def makeMp3(data):
-    """Make mp3 from the quote text, so we know the duration it takes to read"""
-    save_as = "speech.mp3"
-    tts = gtts.gTTS(data, lang='en', tld='ca')
-    tts.save(save_as)
-    return save_as
+# def makeMp3(data):
+#     """Make mp3 from the quote text, so we know the duration it takes to read"""
+#     save_as = "tempFiles/speech.mp3"
+#     tts = gtts.gTTS(data, lang='en', tld='ca')
+#     tts.save(save_as)
+#     return save_as
 
-
-def createVideo(quoteText: str, bgMusic, bgVideo, video_number: int):
-    """Creates the entire video with everything together - this should be split up in different methods"""
-    introText = ['A quote about never giving up on your dreams','A quote about being yourself','A quote about believing in yourself','A quote about making your dreams come true','A quote about happiness','A quote to remind you to stay positive','A quote about never giving up', 'A quote about being grateful', 'A quote about taking risks', 'A quote about living your best life']
-    print("Introtext we will use: ", introText[video_number])
-    # Create intro start
+def videoIntro(introText, videoNumber) -> CompositeVideoClip:
     intro_text_clip = TextClip(
-        txt=introText[video_number],
+        txt=introText[videoNumber],
         fontsize=70,
         size=(800, 0),
         font="Roboto-Regular",
         color="white",
         method="caption",
-        #bg_color='black',
         ).set_position('center')
     
     intro_width, intro_height = intro_text_clip.size
@@ -109,9 +112,14 @@ def createVideo(quoteText: str, bgMusic, bgVideo, video_number: int):
     intro_clip_duration = 6
     text_with_bg= CompositeVideoClip([intro_color_clip, intro_text_clip]).set_position(lambda t: ('center', 200+t)).set_duration(intro_clip_duration)
     intro_final = CompositeVideoClip([intro_clip, text_with_bg]).set_duration(intro_clip_duration)
-    # Create intro end
+    return intro_final
 
-    #quoteArray = ['Happiness is not a brilliant climax to years of grim struggle and anxiety.', 'It is a long succession of little decisions simply to be happy in the moment.']
+def createVideo(quoteText: str, bgMusic: str, bgVideo: str, videoNumber: int):
+    """Creates the entire video with everything together - this should be split up in different methods"""
+    introText = ['A quote about never giving up on your dreams','A quote about being yourself','A quote about believing in yourself','A quote about making your dreams come true','A quote about happiness','A quote to remind you to stay positive','A quote about never giving up', 'A quote about being grateful', 'A quote about taking risks', 'A quote about living your best life']
+    print("Introtext we will use: ", introText[videoNumber])
+    intro_final = videoIntro(introText, videoNumber)
+
     quoteArray = []
     quoteArray.append(quoteText)
     totalTTSTime = 0
@@ -128,6 +136,7 @@ def createVideo(quoteText: str, bgMusic, bgVideo, video_number: int):
         totalTTSTime += time
         #print audio length
         print("Mp3 ", str(idx), " has audio length: ",time)
+
         #createTheClip with the according text
         text_clip = TextClip(
             txt=sentence,
@@ -150,19 +159,21 @@ def createVideo(quoteText: str, bgMusic, bgVideo, video_number: int):
         text_together.audio = new_audioclip
         text_together.fps = 24 #delete this line ??
         completedVideoParts.append(text_together)
-        if os.path.exists(save_as):
-            os.remove(save_as)
-        else:
-            print("Can't find the MP3 file so we did NOT delete it!!")
-    combined_quote_text_with_audio = concatenate_videoclips(completedVideoParts).set_position('center') # delete compose (was needed for resolution issue)
+
+        # if os.path.exists(save_as):
+        #     os.remove(save_as)
+        # else:
+        #     print("Can't find the MP3 file so we did NOT delete it!!")
+
+    combined_quote_text_with_audio = concatenate_videoclips(completedVideoParts).set_position('center') 
     combined_quote_text_with_audio.set_position('center')
 
     #calculate total time
-    total_video_time = intro_clip_duration + totalTTSTime
+    #total_video_time = intro_clip_duration + totalTTSTime
+    total_video_time = intro_final.duration + totalTTSTime
     background_clip = VideoFileClip(bgVideo).resize((1080,1920))
     
     final_export_video = CompositeVideoClip([background_clip, combined_quote_text_with_audio]).subclip(0, totalTTSTime)
-
     #Set audio
     audio_clip = AudioFileClip(bgMusic)
     #change the audio under here...
@@ -174,31 +185,46 @@ def createVideo(quoteText: str, bgMusic, bgVideo, video_number: int):
     final = concatenate_videoclips([intro_final, final_export_video])
     final.audio = new_audioclip
 
-    final.write_videofile("VID_" + str(video_number) + ".mp4", threads=12)
+    final.write_videofile("VID_" + str(videoNumber) + ".mp4", threads=12)
 
 def randomBgMusic():
     """Get a random 'sad' song from the sad_music folder"""
     dir = "sad_music"
-    x = random.choice(os.listdir(dir)) #change dir name to whatever
+    x = random.choice(os.listdir(dir)) 
     print("Random music chosen: ", x)
     return dir + "/" + x
+
+
+def deleteTempFiles():
+    """Deletes the downloaded/generated vid.mp4 and speech.mp3"""
+    print("Deleting temporary downloaded files / generated mp3 file")
+    files = glob.glob('tempFiles/*')
+    for x in files:
+        os.remove(x)
+
+def cleanUpAfterVideoFinished():
+    usedQuoteToDifferentFile()
+    # deleteTempFiles()
+
+def getBackgroundVideo() -> str:
+    scrapedVideosJson = scrapeVideos()
+    videoArray = scrapedVideosJson['videos']
+    randomVideoToScrape = random.randint(0, len(videoArray)-1)
+    videoId = videoArray[randomVideoToScrape]['id']
+    print("Going to scrape video with id: ", videoId)
+    bgVideo = downloadVideo(videoId)
+    return bgVideo
 
 if __name__ == "__main__":
     """Make X amount of videos."""
     for i in range(1): #amount of videos to generate
-        scrapedVideosJson = scrapeVideos()
-        videoArray = scrapedVideosJson['videos']
-        randomVideoToScrape = random.randint(0, len(videoArray)-1)
-        videoId = videoArray[randomVideoToScrape]['id']
-        print("Going to scrape video with id: ", videoId)
-        bgVideo = downloadVideo(videoId)
+        bgVideo = getBackgroundVideo()
         quoteText = getQuote()
-        print(quoteText)
-        deleteFirstLineInTxt() # remove the quote from quotes.txt
-        mp3 = makeMp3(quoteText) # make mp3 and save as: speech.mp3
+        # mp3 = makeMp3(quoteText) # make mp3 and save as: speech.mp3
         bgMusic = randomBgMusic()
-        # createVideo(mp3, quoteText,bgMusic, bgVideo, i) # createVideo(quoteMp3,bgMusic,bgVideo)
-        createVideo(quoteText,bgMusic, bgVideo, i) # createVideo(quoteMp3,bgMusic,bgVideo)
+        #createVideo(mp3, quoteText,bgMusic, bgVideo, i) # createVideo(quoteMp3,bgMusic,bgVideo)
+        createVideo(quoteText, bgMusic, bgVideo, i) # createVideo(quoteMp3,bgMusic,bgVideo)
+        cleanUpAfterVideoFinished()
         print("finished! video: ", i)
     
 
