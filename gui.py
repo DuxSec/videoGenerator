843cfba34d85b97c298cd3b4b3b46a4561d18e88
vosvosvos <indvfx@gmail.com>
2022-11-22 00:17:15 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -11,6 +11,9 @@ class App(ctk.CTk):
         self.title("Video Generator")
         self.geometry(f"{768}x{576}")
         self.minsize(400, 400)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.configure(fg_color="#1c1917")
 
         with open('config.json', 'r') as file:
             self.config_data = json.load(file)
@@ -23,17 +26,49 @@ class App(ctk.CTk):
         if not api_key_set:
             self.setApiKey(text="You've not set your Pexels API Key yet.")
 
-        button = ctk.CTkButton(self,
-                               text="Set API Key",
-                               text_font=("Arial", 12),
-                               command=lambda: self.setApiKey(text="Change your Pexels API Key."))
-        button.grid(row=2, column=0, sticky=ctk.W, padx=(5))
+        self.frame_left = ctk.CTkFrame(master=self,
+                                       width=180,
+                                       fg_color="#292524",
+                                       corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+        self.frame_left.grid_rowconfigure(1, minsize=10)
+        self.frame_left.grid_rowconfigure(3, minsize=10)
+
+        current_config_label = ctk.CTkLabel(self.frame_left,
+                                            text="Current Config",
+                                            text_font=("Helvetica", 14, "bold"),
+                                            width=20,
+                                            height=1,
+                                            anchor=ctk.W,
+                                            corner_radius=0)
+        current_config_label.grid(row=0, column=0, sticky=ctk.W, padx=5, pady=5)
+
+        change_apikey_button = ctk.CTkButton(self.frame_left,
+                                             text="Change API Key",
+                                             text_font=("Helvetica", 10),
+                                             fg_color="#047857",
+                                             hover_color="#059669",
+                                             command=lambda: self.setApiKey(text="Change your Pexels API Key."))
+        change_apikey_button.grid(row=2, column=0, padx=5)
+
+        amount_of_videos_label = ctk.CTkLabel(self.frame_left,
+                                              text="Amount of videos",
+                                              text_font=("Helvetica", 10),
+                                              anchor=ctk.W,)
+        amount_of_videos_label.grid(row=5, column=0, padx=(5))
+
+        amount_of_videos_entry = ctk.CTkEntry(self.frame_left,
+                                              text_font=("Helvetica", 10))
+        amount_of_videos_entry.grid(row=6, column=0, padx=(5))
+        amount_of_videos_entry.insert(
+            0, f"{self.config_data['amountOfVideosToMake']}")
 
     def setApiKey(self, text):
         window = ctk.CTkToplevel(self)
         window.title("Pexels API Key")
         window.geometry(f"{300}x{100}")
         window.minsize(300, 100)
+        window.configure(fg_color="#1c1917")
 
         label = ctk.CTkLabel(window,
                              text=text,
